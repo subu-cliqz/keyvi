9c42b15ca6daecae1b4abb3d528186b7936fa61f
narekgharibyan <narekgharibyan@gmail.com>
2017-09-06 16:33:31 +0200

    - using KeyviString

--- rskeyvi/src/dictionary.rs
+++ rskeyvi/src/dictionary.rs
@@ -1,9 +1,37 @@
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 use std::ffi::{CStr, CString};
+use std::ops::Deref;
 
 
 pub struct Dictionary {
     dict: *mut root::keyvi_dictionary,
+}
+
+struct KeyviString {
+    str_: *mut ::std::os::raw::c_char,
+}
+
+impl KeyviString {
+    fn new(str: *mut ::std::os::raw::c_char) -> KeyviString {
+        KeyviString { str_: str }
+    }
+}
+
+impl Drop for KeyviString {
+    fn drop(&mut self) {
+        unsafe {
+            root::keyvi_string_destroy(self.str_);
+        }
+    }
+}
+
+impl Deref for KeyviString {
+    type Target = str;
+
+    fn deref<'a>(&'a self) -> &'a str {
+        let c_str = unsafe { CStr::from_ptr(self.str_) };
+        c_str.to_str().unwrap()
+    }
 }
 
 impl Dictionary {
@@ -16,11 +44,7 @@ impl Dictionary {
 
     pub fn statistics(&self) -> String {
         let c_buf: *mut ::std::os::raw::c_char = unsafe { root::keyvi_dictionary_get_statistics(self.dict) };
-        let c_str: &CStr = unsafe { CStr::from_ptr(c_buf) };
-        let str_slice: &str = c_str.to_str().unwrap();
-        let str_buf: String = str_slice.to_owned();
-        unsafe { root::keyvi_string_destroy(c_buf); }
-        str_buf
+        KeyviString::new(c_buf).to_owned()
     }
 
     pub fn size(&self) -> u64 {
